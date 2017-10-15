9843f3efa3051c6017f2d6f644ed0ad8ded1084e
Dhanashree Gaonkar <dgaonkar17@gmail.com>
2017-10-14 19:41:37 -0700

    Add files via upload

---
+++ AVLTree/BinaryNode.h
@@ -0,0 +1,32 @@
+/*
+ * BinaryNode.h
+ *
+ *  Created on: May 2, 2017
+ *      Author: Dhanashree S Gaonkar
+ */
+
+/**
+ * A node of a binary tree.
+ */
+
+#ifndef BINARY_NODE_H
+#define BINARY_NODE_H
+
+class BinaryNode
+{
+public:
+    BinaryNode(const int data);
+    BinaryNode(const int data, BinaryNode *left, BinaryNode *right);
+    virtual ~BinaryNode();
+
+    int data;
+    int height;  // node height
+
+    BinaryNode *left;
+    BinaryNode *right;
+};
+
+
+#endif // BINARY_TREE_PRINTER_H
+
+
